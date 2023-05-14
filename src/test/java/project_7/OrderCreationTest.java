b357620a0613c6297ca053dd7e2406b93b943228
Andrei Beliavtsev <buggisart@gmail.com>
2023-05-14 15:53:18 +0400

    delete testng dependencies

--- src/test/java/project_7/OrderCreationTest.java
+++ src/test/java/project_7/OrderCreationTest.java
@@ -3,17 +3,25 @@ package project_7;
 import com.google.gson.Gson;
 import io.restassured.response.ValidatableResponse;
 import io.qameta.allure.Description;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 
 import static org.hamcrest.Matchers.*;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class OrderCreationTest {
+    private final OrderData orderData;
+    private final int expectedStatus;
 
-    @DataProvider(name = "orderData")
-    public Object[][] getOrderData() {
+    public OrderCreationTest(OrderData orderData, int expectedStatus) {
+        this.orderData = orderData;
+        this.expectedStatus = expectedStatus;
+    }
+    @Parameterized.Parameters
+    public static Object[][] getOrderData() {
         return new Object[][]{
                 {new OrderData("HIn","uga","Konoha, 3 apt.", "1","+7 810 355 35 35", 3, "2021-06-10","I'm always cheering for you", new String[]{"BLACK,GREY"}), 201},
                 {new OrderData("Test","tesg","Konoha, 5 apt.", "4","+7 860 355 35 35", 4, "2023-06-10","always cheering for you", new String[]{"BLACK"}), 201},
@@ -22,9 +30,9 @@ public class OrderCreationTest {
         };
     }
 
-    @Test(dataProvider = "orderData")
+    @Test
     @Description("Создание ордера")
-    public void testCreateOrder(OrderData orderData, int expectedStatus) {
+    public void testCreateOrder() {
         Gson gson = new Gson();
         String jsonBody = gson.toJson(orderData);
         CreateOrder createOrder = new CreateOrder();
